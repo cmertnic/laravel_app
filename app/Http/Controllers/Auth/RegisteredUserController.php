db48139437021bcce458868e0eea2d4bfe0b1b40
cmertnic <99240060+cmertnic@users.noreply.github.com>
2025-04-07 15:30:56 +0500

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
-
+use Illuminate\Support\Facades\Storage;
 class RegisteredUserController extends Controller
 {
     /**
