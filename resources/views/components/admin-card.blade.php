ee0cc271e2dda0d05736cf2c40f6f9b782514a93
cmertnic <99240060+cmertnic@users.noreply.github.com>
2025-05-18 13:23:24 +0500

    Update admin-card.blade.php

--- resources/views/components/admin-card.blade.php
+++ resources/views/components/admin-card.blade.php
@@ -1,36 +1,23 @@
 <div class="flex justify-center items-center bg-gray-100">
     <div class="bg-white shadow-md rounded-lg p-4 max-w-md w-full mb-2">
         <h2 class="font-bold text-xl mb-2">Заявка от {{ $report->created_at ? $report->created_at->format('d.m.Y') : 'Неизвестное время' }}</h2>
-        <p><strong>Клиент:</strong> {{ $report->client->name ?? 'Неизвестный клиент' }}</p> 
+        <p><strong>Клиент:</strong> {{ $report->client->name ?? 'Неизвестный клиент' }}</p>
         <p><strong>Время:</strong> {{ $report->time }}</p>
-            <select id="statusSelect-{{ $report->id }}" name="statues_id" onchange="updateStatus(this, '{{ $report->id }}');">
+        @if($report->canBeUpdated) 
+            <select id="statusSelect-{{ $report->id }}" name="status_id" onchange="updateStatus(this, '{{ $report->id }}');">
                 @foreach($statues as $status)
                     <option value="{{ $status->id }}" {{ $report->statues_id == $status->id ? 'selected' : '' }}>{{ $status->name }}</option>
                 @endforeach
             </select>
-        </p>
+        @else
+            <p>Статус не может быть изменен.</p>
+        @endif
     </div>
 </div>
 
 <script>
 function updateStatus(selectElement, reportId) {
     const selectedValue = selectElement.value;
-    let textColor;
-
-    switch (selectedValue) {
-        case '1':
-            textColor = 'black'; 
-            break;
-        case '2':
-            textColor = 'blue'; 
-            break;
-        case '3':
-            textColor = 'red'; 
-            break;
-        default:
-            textColor = 'black';
-    }
-    selectElement.style.color = textColor;
 
     const formData = new FormData();
     formData.append('status_id', selectedValue);
